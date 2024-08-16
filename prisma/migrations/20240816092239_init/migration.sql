be0f4118dc319e361242311da2090c69083d0fe7
Antoine <5269819+Bassintag@users.noreply.github.com>
2024-08-16 11:23:06 +0200

    chore: squish migrations

--- prisma/migrations/20240816092239_init/migration.sql
+++ prisma/migrations/20240816092239_init/migration.sql
@@ -64,6 +64,38 @@ CREATE TABLE "Symbol" (
     CONSTRAINT "Symbol_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "ShoppingList" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ShoppingList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShoppingItem" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "name" TEXT NOT NULL,
+    "purchased" BOOLEAN NOT NULL DEFAULT false,
+    "order" SMALLINT NOT NULL,
+    "shoppingListId" UUID NOT NULL,
+    "ingredientId" UUID,
+
+    CONSTRAINT "ShoppingItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_RecipeToShoppingList" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_RecipeToShoppingList_AB_unique" ON "_RecipeToShoppingList"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_RecipeToShoppingList_B_index" ON "_RecipeToShoppingList"("B");
+
 -- AddForeignKey
 ALTER TABLE "Step" ADD CONSTRAINT "Step_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -78,3 +110,15 @@ ALTER TABLE "RecipeIngredient" ADD CONSTRAINT "RecipeIngredient_ingredientId_fke
 
 -- AddForeignKey
 ALTER TABLE "Symbol" ADD CONSTRAINT "Symbol_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShoppingItem" ADD CONSTRAINT "ShoppingItem_ingredientId_fkey" FOREIGN KEY ("ingredientId") REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShoppingItem" ADD CONSTRAINT "ShoppingItem_shoppingListId_fkey" FOREIGN KEY ("shoppingListId") REFERENCES "ShoppingList"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RecipeToShoppingList" ADD CONSTRAINT "_RecipeToShoppingList_A_fkey" FOREIGN KEY ("A") REFERENCES "Recipe"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RecipeToShoppingList" ADD CONSTRAINT "_RecipeToShoppingList_B_fkey" FOREIGN KEY ("B") REFERENCES "ShoppingList"("id") ON DELETE CASCADE ON UPDATE CASCADE;
