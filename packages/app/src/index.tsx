e945f6eb77ba06a6551c576bd10aa8157be62ca7
Antoine <5269819+Bassintag@users.noreply.github.com>
2024-08-19 17:30:00 +0200

    feat: added persist

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,16 +1,19 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/queryClient";
+import { persister, queryClient } from "@/queryClient";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      persistOptions={{ persister }}
+      client={queryClient}
+    >
       <RouterProvider router={router} />
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 );
--- packages/app/src/queryClient.ts
+++ packages/app/src/queryClient.ts
@@ -1,3 +1,14 @@
 import { QueryClient } from "@tanstack/react-query";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
+
+export const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
